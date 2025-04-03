bd9c6028af832d838ff997685d4bed75d65bb529
Juanjo0828 <jjcastrillongiraldo@gmail.com>
2025-04-03 16:47:00 -0500

    se definen variables y se captura información

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        System.out.println("Hello, World!"); 
+        Scanner sc = new Scanner(System.in);
+        string NombreEmpleado="";
+        string documentoEmpleado = "";
+        string diadescanso = "";
+        double salarioBruto = 0;
+        double salarioNeto = 0;
+        double educcionPension =0;
+        double deduccionSalud =0;
+        double horasTrabajadas =0;
+        double valorHora =0;
+        double bonificacionEmpleado =0;
+        double valorSalarioMinimo = 1423500;
+        double auxilioTransporte =200000;
+        double valorHoraExtra =0;
+
+        System.out.println("ingrese el nombre del empleado:");
+        NombreEmpleado= sc.nextLine();
+        System.out.println("ingrese el documento del empleado:");
+        documentoEmpleado= sc.nextLine();
+        System.out.println("ingrese los dias de descanso:");
+        diadescanso= sc.nextLine();
+
+
+}      
+
+
 }
+
